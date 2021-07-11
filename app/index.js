e73945cc170d67c6b69bd651874d31f9953743ab
felix <felixliawii@gmail.com>
2021-07-11 17:34:28 +0800

    edit text

--- app/index.js
+++ app/index.js
@@ -2,6 +2,6 @@
 const express = require("express");
 const app = express();
 app.get("/", (req, res) => {
-  res.send("Hello World from docker!");
+  res.send("Hello from docker after rebuild!");
 });
 app.listen(5000, () => console.log("Server is up and running"));
